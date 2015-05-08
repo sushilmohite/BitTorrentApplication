05483e48435c65e1e73a500067ac70509136db9c
sushilmohite <sushilmohite@hotmail.com>
2015-05-08 09:40:42 -0400

    Updated Tracker with design changes

--- BitTorrent/src/tracker/Tracker.java
+++ BitTorrent/src/tracker/Tracker.java
@@ -1,11 +1,9 @@
 package tracker;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -17,153 +15,114 @@ import java.util.Scanner;
 
 public class Tracker {
 
-	private final static int port = 1818;
-	public static String backupTrackerIP;
-	Scanner sc;
-	public static Map<String, List<String>> clientInfo = new HashMap<String, List<String>>();
-
+	final private static int PORT = 1818;
+	private String backupTrackerIP;
+	private Map<String, List<String>> clientInfo;
+	
 	public Tracker() {
-		sc = new Scanner(System.in);
-		System.out.println("Enter backup tracker IP");
-		this.backupTrackerIP = sc.nextLine();
+		clientInfo = new HashMap<String, List<String>>();
 	}
 
 	public static void main(String[] args) {
+		
+		Tracker tracker = new Tracker();
 		try {
-			ServerSocket ss = new ServerSocket(port);
-			Tracker t = new Tracker();
+			System.out.println("Tracker running on " + InetAddress.getLocalHost().getHostAddress());
+			System.out.println("Port: " + PORT);
+		} catch (UnknownHostException ex) {
+			System.out.println(ex.getMessage());
+		}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter backup tracker IP");
+		tracker.backupTrackerIP = sc.nextLine();
+		sc.close();
+		
+		tracker.run();
+	}
+	
+	private void run() {
+		try {
+			@SuppressWarnings("resource")
+			ServerSocket serverSocket = new ServerSocket(PORT);
 			while (true) {
-				Socket s = ss.accept();
-				BufferedReader br = new BufferedReader(new InputStreamReader(
-						s.getInputStream()));
-				String param = (br.readLine());
-				t.new ManageRequest(s, param).start();
-				Thread.sleep(100);
+				new ManageRequest(serverSocket.accept()).start();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public class ManageRequest extends Thread {
+	private class ManageRequest extends Thread {
 
 		private Socket socket;
-		BufferedWriter bufferedWriter;
 
-		// the function that is to be called
-		int function;
-
-		// list of parameters for that function
-		String[] param;
-
-		public ManageRequest(Socket socket, String param) {
+		public ManageRequest(Socket socket) {
 			this.socket = socket;
-			this.param = param.split(" ");
-			this.function = Integer.parseInt(this.param[0]);
 		}
 
-		public boolean addClient(String hashValue, String clientIP,
-				int addToBackUp) throws UnknownHostException, IOException {
+		private boolean addClient(String hashValue, String clientIP, boolean addToBackUp) throws Exception {
 			boolean success = false;
 
-			if ((hashValue != null || hashValue != "")
-					&& (clientIP != null || clientIP != "")) {
-				if (Tracker.clientInfo.containsKey(hashValue)) {
-					Tracker.clientInfo.get(hashValue).add(clientIP);
-					success = true;
+			if (hashValue != null && !hashValue.equals("") && clientIP != null && !clientIP.equals("")) {
+				if (clientInfo.containsKey(hashValue)) {
+					clientInfo.get(hashValue).add(clientIP);
 				} else {
 					List<String> clients = new ArrayList<String>();
 					clients.add(clientIP);
-					Tracker.clientInfo.put(hashValue, clients);
-					success = true;
+					clientInfo.put(hashValue, clients);
 				}
-			}
-
-			if (addToBackUp == 1) {
-				Socket backupSocket = new Socket(Tracker.backupTrackerIP,
-						Tracker.port);
-				BufferedWriter bufferedWriter = new BufferedWriter(
-						new OutputStreamWriter(backupSocket.getOutputStream()));
-				String paramBackUp = "2" + " " + hashValue + " " + clientIP
-						+ " " + "0";
-				bufferedWriter.write(paramBackUp);
-				bufferedWriter.flush();
-			}
-
-			return success;
-
-		}
-
-		public List<String> getClients(String hashValue) {
-			List<String> clientList = new ArrayList<String>();
-			if (Tracker.clientInfo.containsKey(hashValue)) {
-				clientList = Tracker.clientInfo.get(hashValue);
-			}
-			return clientList;
-		}
-
-		public boolean addFile(String hashValue, String clientIP,
-				int addToBackUp) throws UnknownHostException, IOException {
-			boolean success = false;
-			if (!Tracker.clientInfo.containsKey(hashValue)) {
-				List<String> clients = new ArrayList<String>();
-				clients.add(clientIP);
-				Tracker.clientInfo.put(hashValue, clients);
 				success = true;
 			}
 
-			if (addToBackUp == 1) {
-				Socket backupSocket = new Socket(Tracker.backupTrackerIP,
-						Tracker.port);
-				BufferedWriter bufferedWriter = new BufferedWriter(
-						new OutputStreamWriter(backupSocket.getOutputStream()));
-				String paramBackUp = "1" + " " + hashValue + " " + clientIP
-						+ " 0";
-				bufferedWriter.write(paramBackUp);
-				bufferedWriter.flush();
+			if (addToBackUp) {
+				Socket backupSocket = new Socket(backupTrackerIP, PORT);
+				PrintWriter out = new PrintWriter(backupSocket.getOutputStream(), true);
+				String paramBackUp = "1" + " " + hashValue + " " + clientIP	+ " " + "false";
+				out.println(paramBackUp);
+				out.close();
+				backupSocket.close();
 			}
+
 			return success;
+
+		}
+
+		private List<String> getClients(String hashValue) {
+			if (clientInfo.containsKey(hashValue)) {
+				return clientInfo.get(hashValue);
+			}
+			return null;
 		}
 
 		public void run() {
-			// String param[] = null;
 
-			switch (function) {
-			case 1:
-				try {
-					addFile(param[1], param[2], Integer.parseInt(param[3]));
-				} catch (NumberFormatException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (UnknownHostException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+			try {
+				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String input = br.readLine();
+				String[] params = input.split(" ");
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			
+			
+				switch (Integer.parseInt(params[0])) {
+				
+				case 1:
+					out.println(addClient(params[1], params[2], Boolean.parseBoolean(params[3])));
+					break;
+				
+				case 2:
+					out.println(getClients(params[1]));
+					break;
+			
+				default:
+					System.out.println("Invalid input: " + input + " from " + socket.getInetAddress().getHostAddress());
+					break;
 				}
-				break;
-			case 2:
-				try {
-					addClient(param[1], param[2], Integer.parseInt(param[3]));
-				} catch (NumberFormatException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (UnknownHostException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				break;
-			case 3:
-				getClients(param[1]);
-				break;
+			
+				br.close();
+				out.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
 			}
-
 		}
 	}
 }
